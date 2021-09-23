0303d75a635ac87e6034c38554bddf3da7f0f47c
HossamElkordi <hossam_elkordi@yahoo.com>
2021-09-23 16:18:25 +0200

    Adding Basic Encoder

--- encoder.py
+++ encoder.py
@@ -1,12 +1,27 @@
-import argparse
+import torch
+import json
 from torchvision import models as models
+from tqdm import tqdm
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Image Captioning Model')
-    parser.add_argument('--model', type=str, default='model.pth')
-    args = parser.parse_args()
+class Encoder():
+    def __init__(self, model_path, out_path, device):
+        self.device = device
+        self.out_path = out_path
+        self.vgg = models.vgg16()
+        self.vgg.classifier = self.vgg.classifier[:-3]
+        self.vgg.load_state_dict(torch.load(model_path, map_location=device))
+        self.vgg.to(self.device)
 
-    vgg = models.vgg16()
-    vgg.classifier = vgg.classifier[:-3]
-    vgg.load_state_dict(args.model)
+    def extract_features(self, model, dataloader):
+        model.eval()
+        features_map = {}
+        with tqdm(desc='Evaluation', unit='it', total=len(dataloader)) as pbar:
+            for it, (ids, images) in enumerate(iter(dataloader)):
+                images = images.to(self.device)
+                features = model(images)
+                for id_i, f_i in enumerate(zip(ids, features)):
+                    features_map[id_i] = f_i
+            pbar.update()
+        json.dump(features_map, open(self.out_path, 'w'))
+        return features_map
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import argparse
+import torch
+from encoder import Encoder
 
+if __name__ == '__main__':
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    parser = argparse.ArgumentParser(description='Image Captioning Model')
+    parser.add_argument('--model', type=str, default='model.pth')
+    parser.add_argument('--out_features', type=str, default='features.json')
+    args = parser.parse_args()
+
+    encoderModule = Encoder(model_path=args.model, out_path=args.out_features, device=device)

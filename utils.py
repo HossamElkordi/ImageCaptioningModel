dfa869d136a49e10d5cc12f4d14dd4e98d4772ca
HossamElkordi <hossam_elkordi@yahoo.com>
2021-09-25 02:05:35 +0200

    Finishing Encoder

--- utils.py
+++ utils.py
@@ -1,16 +1,17 @@
-import json
+import torch
 from tqdm import tqdm
 
 
-def extract_features(model, dataloader, device, out_path='features.json'):
+def extract_features(model, dataloader, device, out_path='features.pt'):
+    import itertools
     model.eval()
     features_map = {}
-    with tqdm(desc='Evaluation', unit='it', total=len(dataloader)) as pbar:
+    with tqdm(desc='Extracting Features', unit='it', total=len(dataloader)) as pbar:
         for it, (ids, images) in enumerate(iter(dataloader)):
             images = images.to(device)
             features = model(images)
             for id_i, f_i in enumerate(zip(ids, features)):
                 features_map[id_i] = f_i
         pbar.update()
-    json.dump(features_map, open(out_path, 'w'))
+    torch.save(features_map, open(out_path, 'w'))
     return features_map
